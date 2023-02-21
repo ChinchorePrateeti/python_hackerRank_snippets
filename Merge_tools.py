143f9aee3af4686b81d587c0193e545d9ce0c981
prateeti chinchore <ptt.chinchore@gmail.com>
2023-02-22 01:17:29 +0530

    Update Merge_tools.py

--- Merge_tools.py
+++ Merge_tools.py
@@ -1,10 +1,9 @@
 def merge_the_tools(string, k):
     # inding length of string and substring
     length_of_string = len(string) 
-    length_of_substring = int(length_of_string/k)
-    for index in range(0, length_of_string, length_of_substring):
+    for index in range(0, length_of_string, k):
         #set method returns the unique values
-        substring = list(set(string[index:index+length_of_substring]))
+        substring = list(set(string[index:index+k]))
         #converting list to string again
         substring ="".join(substring)
         print(substring)
